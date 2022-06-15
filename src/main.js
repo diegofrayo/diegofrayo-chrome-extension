5162c25bc14ca65c5d0d643f96973bd02edc7ab8
Diego Rayo <diegofrayo@gmail.com>
2022-06-14 21:20:22 -0500

    :sparkles: [minor] improvements

--- src/main.js
+++ src/main.js
@@ -1,27 +1,40 @@
 document.addEventListener("DOMContentLoaded", () => {
-  document
-    .getElementsByTagName("button")[0]
-    .addEventListener("click", async function handleCopyPageTitleClick() {
-      const $textNode = document.getElementsByTagName("p")[0];
+  const buttons = document.getElementsByTagName("button");
 
-      try {
-        $textNode.innerHTML = "";
-
-        chrome.tabs.getSelected(null, async function (tab) {
-          const url = new URL(tab.url);
-
-          await navigator.clipboard.writeText(
-            `**[${tab.title} | [${url.host.replace("www.", "")}]](${tab.url})**`
-          );
-
-          $textNode.innerHTML = "copied";
-          setTimeout(() => {
-            $textNode.innerHTML = "";
-          }, 2000);
-        });
-      } catch (error) {
-        console.error(error);
-        $textNode.innerHTML = error.message;
-      }
-    });
+  buttons[0].addEventListener("click", handleButtonClick("no-search-params"));
+  buttons[1].addEventListener("click", handleButtonClick("with-search-params"));
 });
+
+function handleButtonClick(config) {
+  return async function handleButtonClick() {
+    const $textNode = document.getElementsByTagName("p")[0];
+
+    try {
+      $textNode.innerHTML = "";
+
+      chrome.tabs.getSelected(null, async function (tab) {
+        const url = new URL(tab.url);
+
+        const textToCopy = `**[${tab.title} | [${url.host.replace(
+          "www.",
+          ""
+        )}]](${
+          config === "no-search-params"
+            ? `${url.origin}${url.pathname}`
+            : url.href
+        })**`;
+
+        await navigator.clipboard.writeText(textToCopy);
+        console.log(textToCopy);
+
+        $textNode.innerHTML = "copied";
+        setTimeout(() => {
+          $textNode.innerHTML = "";
+        }, 2000);
+      });
+    } catch (error) {
+      console.error(error);
+      $textNode.innerHTML = error.message;
+    }
+  };
+}
