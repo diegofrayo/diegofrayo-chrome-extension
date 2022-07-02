9fdd36475cf87ff3a36582392275567f00909cdb
Diego Rayo <diegofrayo@gmail.com>
2022-07-02 18:27:57 -0500

    :sparkles: [add] more options for copying urls or create bookmarks for notion

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
+const OPTIONS = {
+  URL: "URL",
+  NOTION: "NOTION",
+  NOTION_WITH_SEARCH_PARAMS: "NOTION_WITH_SEARCH_PARAMS",
+  MY_WEBSITE: "MY_WEBSITE",
+};
+
 document.addEventListener("DOMContentLoaded", () => {
   const buttons = document.getElementsByTagName("button");
 
-  buttons[0].addEventListener("click", handleButtonClick("no-search-params"));
-  buttons[1].addEventListener("click", handleButtonClick("with-search-params"));
+  buttons[0].addEventListener("click", handleButtonClick(OPTIONS.URL));
+  buttons[1].addEventListener("click", handleButtonClick(OPTIONS.NOTION));
+  buttons[2].addEventListener(
+    "click",
+    handleButtonClick(OPTIONS.NOTION_WITH_SEARCH_PARAMS)
+  );
+  buttons[3].addEventListener("click", handleButtonClick(OPTIONS.MY_WEBSITE));
 });
 
 function handleButtonClick(config) {
@@ -14,15 +26,32 @@ function handleButtonClick(config) {
 
       chrome.tabs.getSelected(null, async function (tab) {
         const url = new URL(tab.url);
+        let textToCopy = "";
 
-        const textToCopy = `**[${tab.title} | [${url.host.replace(
-          "www.",
-          ""
-        )}]](${
-          config === "no-search-params"
-            ? `${url.origin}${url.pathname}`
-            : url.href
-        })**`;
+        if (config === OPTIONS.URL) {
+          textToCopy = `${url.origin}${url.pathname}`;
+        } else if (config === OPTIONS.MY_WEBSITE) {
+          const newDate = new Date();
+          const addPadding = (number) => (number < 10 ? `0${number}` : number);
+
+          textToCopy = JSON.stringify({
+            title: tab.title,
+            url: url.href.replace("www.", ""),
+            date: `${newDate.getFullYear()}/${addPadding(
+              newDate.getMonth() + 1
+            )}/${addPadding(newDate.getDate())}`,
+            done: false,
+            starred: false,
+          });
+        } else {
+          const isYouTubeVideo = url.href.includes("youtube.com");
+
+          textToCopy = `**[${tab.title} | [${url.host.replace("www.", "")}]](${
+            config === OPTIONS.NOTION_WITH_SEARCH_PARAMS || isYouTubeVideo
+              ? url.href
+              : `${url.origin}${url.pathname}`
+          })**`;
+        }
 
         await navigator.clipboard.writeText(textToCopy);
         console.log(textToCopy);
