b2058a497e5ed68035b8361c0e4c8aa90507aad5
Diego Rayo <diegofrayo@gmail.com>
2022-07-09 13:47:31 -0500

    :sparkles: [add] button to copy a bookmark for music pages

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ const OPTIONS = {
   NOTION: "NOTION",
   NOTION_WITH_SEARCH_PARAMS: "NOTION_WITH_SEARCH_PARAMS",
   MY_WEBSITE: "MY_WEBSITE",
+  MY_WEBSITE_MUSIC_PAGE: "MY_WEBSITE_MUSIC_PAGE",
 };
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -15,16 +16,20 @@ document.addEventListener("DOMContentLoaded", () => {
     handleButtonClick(OPTIONS.NOTION_WITH_SEARCH_PARAMS)
   );
   buttons[3].addEventListener("click", handleButtonClick(OPTIONS.MY_WEBSITE));
+  buttons[4].addEventListener(
+    "click",
+    handleButtonClick(OPTIONS.MY_WEBSITE_MUSIC_PAGE)
+  );
 });
 
 function handleButtonClick(config) {
   return async function handleButtonClick() {
-    const $textNode = document.getElementsByTagName("p")[0];
-
-    try {
+    chrome.tabs.getSelected(null, async function (tab) {
+      const $textNode = document.getElementsByTagName("p")[0];
       $textNode.innerHTML = "";
 
-      chrome.tabs.getSelected(null, async function (tab) {
+      try {
+        const isYouTubeVideo = tab.url.includes("youtube.com");
         const url = new URL(tab.url);
         let textToCopy = "";
 
@@ -43,14 +48,31 @@ function handleButtonClick(config) {
             done: false,
             starred: false,
           });
-        } else {
-          const isYouTubeVideo = url.href.includes("youtube.com");
-
+        } else if (config.includes("NOTION")) {
           textToCopy = `**[${tab.title} | [${url.host.replace("www.", "")}]](${
-            config === OPTIONS.NOTION_WITH_SEARCH_PARAMS || isYouTubeVideo
+            isYouTubeVideo
+              ? `https://youtu.be/${url.searchParams.get("v")}`
+              : config === OPTIONS.NOTION_WITH_SEARCH_PARAMS
               ? url.href
               : `${url.origin}${url.pathname}`
           })**`;
+        } else if (config === OPTIONS.MY_WEBSITE_MUSIC_PAGE) {
+          textToCopy = JSON.stringify({
+            order: 0,
+            text: isYouTubeVideo
+              ? tab.title.replace(" - YouTube", "")
+              : tab.title,
+            url: isYouTubeVideo
+              ? `https://youtu.be/${url.searchParams.get("v")}`
+              : `${url.origin}${url.pathname}`,
+            source: isYouTubeVideo
+              ? "youtube"
+              : url.href.includes("lacuerda")
+              ? "lacuerda"
+              : url.href.includes("instagram")
+              ? "instagram"
+              : "url",
+          });
         }
 
         await navigator.clipboard.writeText(textToCopy);
@@ -60,10 +82,10 @@ function handleButtonClick(config) {
         setTimeout(() => {
           $textNode.innerHTML = "";
         }, 2000);
-      });
-    } catch (error) {
-      console.error(error);
-      $textNode.innerHTML = error.message;
-    }
+      } catch (error) {
+        console.error(error);
+        $textNode.innerHTML = error.message;
+      }
+    });
   };
 }
