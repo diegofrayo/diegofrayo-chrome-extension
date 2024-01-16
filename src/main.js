b162991425bd5de784f11a409a6af0bfbccbb005
Diego Rayo <diegofrayo@gmail.com>
2024-01-16 12:09:41 -0500

    :sparkles: [add] compatibility with firefox

--- src/main.js
+++ src/main.js
@@ -1,274 +1,272 @@
 // --- VARS ---
 
 const OPTIONS = {
-  NOTION: "NOTION",
-  TITLE: "TITLE",
-  URL: "URL",
-  WEBSITE_MUSIC_PAGE: "WEBSITE_MUSIC_PAGE",
-  WEBSITE_CONTACTS_PAGE: "WEBSITE_CONTACTS_PAGE",
-  WEBSITE_FILMS_PAGE: "WEBSITE_FILMS_PAGE",
-  WEBSITE_BOOKS_PAGE: "WEBSITE_BOOKS_PAGE",
-  YOUTUBE_OR_SPOTIFY_ID: "YOUTUBE_OR_SPOTIFY_ID",
+	NOTION: "NOTION",
+	TITLE: "TITLE",
+	URL: "URL",
+	WEBSITE_MUSIC_PAGE: "WEBSITE_MUSIC_PAGE",
+	WEBSITE_CONTACTS_PAGE: "WEBSITE_CONTACTS_PAGE",
+	WEBSITE_FILMS_PAGE: "WEBSITE_FILMS_PAGE",
+	WEBSITE_BOOKS_PAGE: "WEBSITE_BOOKS_PAGE",
+	YOUTUBE_OR_SPOTIFY_ID: "YOUTUBE_OR_SPOTIFY_ID",
 };
 let errorTimeout = null;
 
 // --- LISTENERS ---
 
 document.addEventListener("DOMContentLoaded", () => {
-  // --- BUTTONS SETUP ---
-  const buttons = document.getElementsByTagName("button");
-  const optionsKeys = Object.keys(OPTIONS);
+	// --- BUTTONS SETUP ---
+	const buttons = document.getElementsByTagName("button");
+	const optionsKeys = Object.keys(OPTIONS);
 
-  for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener("click", handleButtonClick(optionsKeys[i]));
-  }
+	for (let i = 0; i < buttons.length; i++) {
+		buttons[i].addEventListener("click", handleButtonClick(optionsKeys[i]));
+	}
 
-  // --- CHECKBOXES SETUP ---
-  const checkboxes = document.querySelectorAll("input[type='checkbox']");
+	// --- CHECKBOXES SETUP ---
+	const checkboxes = document.querySelectorAll("input[type='checkbox']");
 
-  for (let i = 0; i < checkboxes.length; i++) {
-    checkboxes[i].addEventListener("change", onCheckboxChange);
-    checkboxes[i].checked =
-      stringToBoolean(window.localStorage.getItem(checkboxes[i].id)) || false;
-  }
+	for (let i = 0; i < checkboxes.length; i++) {
+		checkboxes[i].addEventListener("change", onCheckboxChange);
+		checkboxes[i].checked = stringToBoolean(window.localStorage.getItem(checkboxes[i].id)) || false;
+	}
 });
 
 // --- HANDLERS ---
 
 function handleButtonClick(config) {
-  return async function handleButtonClick() {
-    const $textNode = document.getElementsByTagName("p")[0];
-    $textNode.innerHTML = "";
-    $textNode.classList.remove("error");
+	return async function handleButtonClick() {
+		const $textNode = document.getElementsByTagName("p")[0];
+		$textNode.innerHTML = "";
+		$textNode.classList.remove("error");
 
-    try {
-      const [tab] = await chrome.tabs.query({
-        active: true,
-        currentWindow: true,
-      });
-      const withQueryStringsOption = document.getElementById(
-        "checkbox-with-query-strings"
-      ).checked;
+		try {
+			const [tab] = await getBrowser().tabs.query({ currentWindow: true, active: true });
 
-      const isYouTubePage = tab.url.includes("youtube.com");
-      const url = new URL(tab.url);
-      const title = cleanTitle(tab.title, url.href);
-      let textToCopy = "";
+			const withQueryStringsOption = document.getElementById("checkbox-with-query-strings").checked;
 
-      if (config === OPTIONS.TITLE) {
-        textToCopy = title;
-      } else if (config === OPTIONS.URL) {
-        textToCopy = parseURL(url, {
-          withQueryStrings: withQueryStringsOption,
-        });
-      } else if (config === OPTIONS.YOUTUBE_OR_SPOTIFY_ID) {
-        textToCopy = isYouTubePage
-          ? url.searchParams.get("v")
-          : url.pathname.split("/track/")[1];
-      } else if (config === OPTIONS.NOTION) {
-        textToCopy = `**[${title} | [${getHostName(url)}]](${parseURL(url, {
-          withQueryStrings: withQueryStringsOption,
-          isYouTubePage,
-        })})**`;
-      } else if (config === OPTIONS.WEBSITE_MUSIC_PAGE) {
-        textToCopy = JSON.stringify({
-          text: title,
-          url: parseURL(url, {
-            withQueryStrings: withQueryStringsOption,
-            isYouTubePage,
-          }),
-          source: isYouTubePage
-            ? "youtube"
-            : url.href.includes("lacuerda")
-            ? "lacuerda"
-            : url.href.includes("instagram")
-            ? "instagram"
-            : "url",
-        });
-      } else if (config === OPTIONS.WEBSITE_FILMS_PAGE) {
-        const isNetflixFilm = url.href.includes("netflix.com");
-        const id = (
-          (isNetflixFilm
-            ? url.searchParams.get("jbv")
-            : isYouTubePage
-            ? url.searchParams.get("v")
-            : "") || ""
-        ).toLowerCase();
+			const isYouTubePage = tab.url.includes("youtube.com");
+			const url = new URL(tab.url);
+			const title = cleanTitle(tab.title, url.href);
+			let textToCopy = "";
 
-        textToCopy = JSON.stringify({
-          id,
-          title,
-          type: "",
-          source: isNetflixFilm ? "Netflix" : "YouTube",
-          calification: 3,
-          categories: [],
-          added_date: getCurrentDate(),
-          is_public: false,
-          cover: `{{URL}}/assets/images/pages/apps/films/assets/${id}.jpg`,
-        });
-      } else if (config === OPTIONS.WEBSITE_BOOKS_PAGE) {
-        const id = generateSlug(title);
+			if (config === OPTIONS.TITLE) {
+				textToCopy = title;
+			} else if (config === OPTIONS.URL) {
+				textToCopy = parseURL(url, {
+					withQueryStrings: withQueryStringsOption,
+				});
+			} else if (config === OPTIONS.YOUTUBE_OR_SPOTIFY_ID) {
+				textToCopy = isYouTubePage ? url.searchParams.get("v") : url.pathname.split("/track/")[1];
+			} else if (config === OPTIONS.NOTION) {
+				textToCopy = `**[${title} | [${getHostName(url)}]](${parseURL(url, {
+					withQueryStrings: withQueryStringsOption,
+					isYouTubePage,
+				})})**`;
+			} else if (config === OPTIONS.WEBSITE_MUSIC_PAGE) {
+				textToCopy = JSON.stringify({
+					text: title,
+					url: parseURL(url, {
+						withQueryStrings: withQueryStringsOption,
+						isYouTubePage,
+					}),
+					source: isYouTubePage
+						? "youtube"
+						: url.href.includes("lacuerda")
+						? "lacuerda"
+						: url.href.includes("instagram")
+						? "instagram"
+						: "url",
+				});
+			} else if (config === OPTIONS.WEBSITE_FILMS_PAGE) {
+				const isNetflixFilm = url.href.includes("netflix.com");
+				const id = (
+					(isNetflixFilm
+						? url.searchParams.get("jbv")
+						: isYouTubePage
+						? url.searchParams.get("v")
+						: "") || ""
+				).toLowerCase();
 
-        textToCopy = JSON.stringify({
-          id,
-          title,
-          author: "",
-          year: 2000,
-          calification: 3,
-          added_date: getCurrentDate(),
-          url: parseURL(url, {
-            withQueryStrings: withQueryStringsOption,
-            isYouTubePage,
-          }),
-          is_public: false,
-          cover: `{{URL}}/assets/images/pages/apps/books/${id}.jpg`,
-        });
-      } else if (config === OPTIONS.WEBSITE_CONTACTS_PAGE) {
-        textToCopy = JSON.stringify({
-          id: generateSlug(title),
-          name: title,
-          phone: "",
-          instagram: replaceAll(url.pathname, "/", ""),
-          maps: "",
-          menu: "",
-          country: "CO",
-        });
-      }
+				textToCopy = JSON.stringify({
+					id,
+					title,
+					type: "",
+					source: isNetflixFilm ? "Netflix" : "YouTube",
+					calification: 3,
+					categories: [],
+					added_date: getCurrentDate(),
+					is_public: true,
+					cover: `/assets/images/pages/apps/films/assets/${id}.jpg`,
+				});
+			} else if (config === OPTIONS.WEBSITE_BOOKS_PAGE) {
+				const id = generateSlug(title);
 
-      await navigator.clipboard.writeText(textToCopy);
-      console.log(textToCopy);
+				textToCopy = JSON.stringify({
+					id,
+					title,
+					author: "",
+					year: 2000,
+					calification: 3,
+					added_date: getCurrentDate(),
+					url: parseURL(url, {
+						withQueryStrings: withQueryStringsOption,
+						isYouTubePage,
+					}),
+					is_public: true,
+					cover: `/assets/images/pages/apps/books/${id}.jpg`,
+				});
+			} else if (config === OPTIONS.WEBSITE_CONTACTS_PAGE) {
+				textToCopy = JSON.stringify({
+					id: generateSlug(title),
+					name: title,
+					phone: "",
+					instagram: replaceAll(url.pathname, "/", ""),
+					maps: "",
+					menu: "",
+					country: "CO",
+				});
+			}
 
-      $textNode.innerHTML = "Copied to the clipboard!";
+			await navigator.clipboard.writeText(textToCopy);
+			console.log(textToCopy);
 
-      if (errorTimeout) {
-        errorTimeout = clearTimeout(errorTimeout);
-      }
+			$textNode.innerHTML = "Copied to the clipboard!";
 
-      errorTimeout = setTimeout(() => {
-        $textNode.innerHTML = "";
-      }, 2000);
-    } catch (error) {
-      console.error(error);
-      $textNode.innerHTML = `Error: ${error.message}`;
-      $textNode.title = `Error: ${error.message}`;
-      $textNode.classList.add("error");
-    }
-  };
+			if (errorTimeout) {
+				errorTimeout = clearTimeout(errorTimeout);
+			}
+
+			errorTimeout = setTimeout(() => {
+				$textNode.innerHTML = "";
+			}, 2000);
+		} catch (error) {
+			console.error(error);
+			$textNode.innerHTML = `Error: ${error.message}`;
+			$textNode.title = `Error: ${error.message}`;
+			$textNode.classList.add("error");
+		}
+	};
 }
 
 function onCheckboxChange(event) {
-  window.localStorage.setItem(event.target.id, event.target.checked);
+	window.localStorage.setItem(event.target.id, event.target.checked);
 }
 
 // --- UTILS ---
 
-function getHostName(url) {
-  if (url.href.includes("google.com/maps")) {
-    return "maps.google.com";
-  }
+function getBrowser() {
+	if (typeof chrome !== "undefined") {
+		if (typeof browser !== "undefined") {
+			return browser;
+		} else {
+			return chrome;
+		}
+	}
 
-  return url.host.replace("www.", "");
+	throw new Error(`Invalid browser`);
+}
+
+function getHostName(url) {
+	if (url.href.includes("google.com/maps")) {
+		return "maps.google.com";
+	}
+
+	return url.host.replace("www.", "");
 }
 
 function cleanTitle(title, href) {
-  if (title.includes("Fotos y videos de Instagram")) {
-    return title.substring(0, title.lastIndexOf(")")) + ")";
-  }
+	if (title.includes("Fotos y videos de Instagram")) {
+		return title.substring(0, title.lastIndexOf(")")) + ")";
+	}
 
-  if (
-    href.includes(".twitter.com") ||
-    href.includes("/twitter.com") ||
-    href.includes(".x.com") ||
-    href.includes("/x.com")
-  ) {
-    return (
-      title
-        .split(" ")
-        .filter((item) => item.includes("https") === false)
-        .join(" ")
-        .replace(" / X", '"')
-        .replace('""', '"')
-        .split("en X:")[1] || ""
-    ).trim();
-  }
+	if (
+		href.includes(".twitter.com") ||
+		href.includes("/twitter.com") ||
+		href.includes(".x.com") ||
+		href.includes("/x.com")
+	) {
+		return (
+			title
+				.split(" ")
+				.filter((item) => item.includes("https") === false)
+				.join(" ")
+				.replace(" / X", '"')
+				.replace('""', '"')
+				.split("en X:")[1] || ""
+		).trim();
+	}
 
-  return title
-    .replace(" - Netflix", "")
-    .replace(" - YouTube", "")
-    .replace(" - Google Maps", "");
+	return title.replace(" - Netflix", "").replace(" - YouTube", "").replace(" - Google Maps", "");
 }
 
 function parseURL(url, options = { withQueryStrings: false, isYouTubePage }) {
-  if (options.withQueryStrings) {
-    return url.href;
-  }
+	if (options.withQueryStrings) {
+		return url.href;
+	}
 
-  const WHITELIST_PARAMS = ["v", "viewkey", "t"];
-  const params = new URLSearchParams(url.search);
-  const parsedParams = [];
+	const WHITELIST_PARAMS = ["v", "viewkey", "t"];
+	const params = new URLSearchParams(url.search);
+	const parsedParams = [];
 
-  for (let [param, paramValue] of params) {
-    if (WHITELIST_PARAMS.includes(param)) {
-      parsedParams.push(
-        `${encodeURIComponent(param)}=${encodeURIComponent(paramValue)}`
-      );
-    }
-  }
+	for (let [param, paramValue] of params) {
+		if (WHITELIST_PARAMS.includes(param)) {
+			parsedParams.push(`${encodeURIComponent(param)}=${encodeURIComponent(paramValue)}`);
+		}
+	}
 
-  const isYouTubeVideo = params.get("v") !== null;
-  if (options.isYouTubePage && isYouTubeVideo) {
-    return `https://youtu.be/${url.searchParams.get("v")}${
-      url.searchParams.get("t") ? `?t=${url.searchParams.get("t")}` : ""
-    }`;
-  }
+	const isYouTubeVideo = params.get("v") !== null;
+	if (options.isYouTubePage && isYouTubeVideo) {
+		return `https://youtu.be/${url.searchParams.get("v")}${
+			url.searchParams.get("t") ? `?t=${url.searchParams.get("t")}` : ""
+		}`;
+	}
 
-  return `${url.origin}${url.pathname}${
-    url.hash.includes(":~:text=") ? "" : url.hash
-  }${parsedParams.length > 0 ? `?${parsedParams.join("&")}` : ""}`;
+	return `${url.origin}${url.pathname}${url.hash.includes(":~:text=") ? "" : url.hash}${
+		parsedParams.length > 0 ? `?${parsedParams.join("&")}` : ""
+	}`;
 }
 
 function getCurrentDate() {
-  const addPadding = (number) => (number < 10 ? `0${number}` : number);
-  const newDate = new Date();
+	const addPadding = (number) => (number < 10 ? `0${number}` : number);
+	const newDate = new Date();
 
-  return `${newDate.getFullYear()}/${addPadding(
-    newDate.getMonth() + 1
-  )}/${addPadding(newDate.getDate())}`;
+	return `${newDate.getFullYear()}/${addPadding(newDate.getMonth() + 1)}/${addPadding(
+		newDate.getDate(),
+	)}`;
 }
 
 function generateSlug(str) {
-  let result = str.replace(/^\s+|\s+$/g, "").toLowerCase();
+	let result = str.replace(/^\s+|\s+$/g, "").toLowerCase();
 
-  // remove accents, swap ñ for n, etc
-  const FROM = "àáäâèéëêìíïîòóöôùúüûñç·/_,:;";
-  const TO = "aaaaeeeeiiiioooouuuunc------";
+	// remove accents, swap ñ for n, etc
+	const FROM = "àáäâèéëêìíïîòóöôùúüûñç·/_,:;";
+	const TO = "aaaaeeeeiiiioooouuuunc------";
 
-  createArray(FROM.length).forEach((i) => {
-    result = replaceAll(result, FROM.charAt(i), TO.charAt(i));
-  });
+	createArray(FROM.length).forEach((i) => {
+		result = replaceAll(result, FROM.charAt(i), TO.charAt(i));
+	});
 
-  result = result
-    .replace(/[^a-z0-9 -]/g, "") // remove invalid chars
-    .replace(/\s+/g, "-") // collapse whitespace and replace by -
-    .replace(/-+/g, "-"); // collapse dashes
+	result = result
+		.replace(/[^a-z0-9 -]/g, "") // remove invalid chars
+		.replace(/\s+/g, "-") // collapse whitespace and replace by -
+		.replace(/-+/g, "-"); // collapse dashes
 
-  return result;
+	return result;
 }
 
 function createArray(length, start) {
-  return Array.from(Array(length).keys()).map(
-    (value) => value + (start === undefined ? 1 : start)
-  );
+	return Array.from(Array(length).keys()).map((value) => value + (start === undefined ? 1 : start));
 }
 
 function replaceAll(str, toReplace, replacement = "") {
-  return str.replace(new RegExp(escapeRegExp(toReplace), "g"), replacement);
+	return str.replace(new RegExp(escapeRegExp(toReplace), "g"), replacement);
 }
 
 function escapeRegExp(text) {
-  return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+	return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
 }
 
 function stringToBoolean(input) {
-  return input === "true" ? true : false;
+	return input === "true" ? true : false;
 }
